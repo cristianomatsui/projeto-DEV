66e07c586c6a1c62bf8170e142078421d5e7ad9e
Cristiano Matsui <cristiano.matsui@gmail.com>
2019-03-18 09:59:14 -0300

    login form created

---
+++ BetZ/src/app/shared/wmessage/wmessage.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { WMessageComponent } from './WMessage.component';
+
+@NgModule({
+  declarations: [ WMessageComponent ],
+  exports: [ WMessageComponent]
+})
+export class WMessageModule { }
